3819aa44c283be64d4ae4e198e4c6a541cd69004
patcim7 <91811996+patcim7@users.noreply.github.com>
2025-04-07 13:05:59 -0400

    Create auction_closure.py

---
+++ auction_closure.py
@@ -0,0 +1,27 @@
+import socket
+
+from auction_update_server import listed_items, client_bids
+from server import registered_clients # Why it's not working???
+
+
+def getHihestBid(itemName):
+    # Retrieve the highest bid from the dictionary as well as all the info
+    if itemName in listed_items and client_bids[itemName]:
+        highestBid = max(client_bids[itemName], key = client_bids[1])
+        return highestBid
+    return None # if no bids were placed
+
+
+def notifyActionClosure(itemName, RQ):
+    highestBid = getHihestBid(itemName)
+
+    if highestBid is not None:
+        buyerName, finalPrice = highestBid
+        winnerMsg = f"WINNER {RQ} {itemName} {finalPrice} {listed_items[2]}"
+        selletMsg = f"SOLD {RQ} {itemName} {listed_items[3]}"
+        send_tcp_message(buyerName, winnerMsg)
+        send_tcp_message(seller_name, selletMsg)
+
+    else:
+        non_offer_msg = f"NON_OFFER {request_number} {item_name}"
+        send_tcp_message(seller_name, non_offer_msg)
